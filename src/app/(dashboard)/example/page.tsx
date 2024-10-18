8d01084da58422f15b733f447308bd69094c9011
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-18 20:07:35 -0300

    fix: style

--- src/app/(dashboard)/example/page.tsx
+++ src/app/(dashboard)/example/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div className="m-8 flex flex-1 items-center justify-center bg-orange-100 dark:bg-orange-950">content</div>;
+  return <div className="flex flex-1 items-center justify-center bg-orange-100 dark:bg-orange-950">content</div>;
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { readonly children: React.ReactNod
       <div className="flex w-80 items-center justify-center bg-green-100 dark:bg-green-950">sidebar</div>
 
       <header className="flex h-18 items-center justify-center bg-purple-100 dark:bg-purple-950">header</header>
-      <div className="mx-auto flex w-full max-w-8xl flex-1">{children}</div>
+      <div className="mx-auto flex w-full max-w-8xl flex-1 p-8">{children}</div>
     </div>
   );
 }
