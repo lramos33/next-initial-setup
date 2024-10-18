d7d7656034964c7c1a180670916528a01386d109
lramos33 <oliveira.leonardo3004@gmail.com>
2024-10-18 20:03:00 -0300

    fix: style

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,10 +3,8 @@ export default function Layout({ children }: { readonly children: React.ReactNod
     <div className="flex min-h-svh bg-blue-100 dark:bg-blue-950">
       <div className="flex w-80 items-center justify-center bg-green-100 dark:bg-green-950">sidebar</div>
 
-      <div className="flex flex-1 flex-col">
-        <header className="flex h-18 items-center justify-center bg-purple-100 dark:bg-purple-950">header</header>
-        <div className="mx-auto flex size-full max-w-8xl">{children}</div>
-      </div>
+      <header className="flex h-18 items-center justify-center bg-purple-100 dark:bg-purple-950">header</header>
+      <div className="mx-auto flex w-full max-w-8xl flex-1">{children}</div>
     </div>
   );
 }
